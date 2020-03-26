1781d4c713366b6158e76e9e7816f89329a5d50e
italocavalcante <italocavalcantefortaleza@live.com>
2020-03-26 19:35:21 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+// Import of express to configure routes.
+const express = require('express')
+const app = express();
+// Import of body-parser to read form-data
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+
+app.get("/", (req,res) =>{
+    res.sendFile(__dirname + "/html/login.html")
+})
+
+app.post("/post/user/login",(req,res) => {
+    console.log(req.body["username"])
+    console.log(req.body["password"])
+    console.log(req.query["action"])
+})
+
+app.listen(8080,(req, res) => {
+    console.log("Server started")
+})
