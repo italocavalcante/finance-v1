c3c2649b18fc7661f80738074b10b0730e1d34e1
italocavalcante <italocavalcantefortaleza@live.com>
2020-04-19 06:19:39 -0300

    Pagination configure

--- database/expensesController.js
+++ database/expensesController.js
@@ -6,13 +6,57 @@ router.use(bodyParser.urlencoded({extended: false}))
 router.use(bodyParser.json())
 
 router.get("/expenses",(req, res) => {
-    res.render("expenses/expense")
+    //res.render("expenses/expense")
+    Expenses.findAndCountAll({
+
+    }).then((users)=>{
+        res.json(users)
+    })
+})
+
+router.get("/expenses/page/:number",(req, res) => {
+    pageNumber = req.params.number
+    console.log(pageNumber)
+
+    if(pageNumber == 1 || pageNumber < 1 || isNaN(pageNumber)){
+        offset = 0
+    }else{
+        offset = (pageNumber - 1) * 10
+    }
+
+    Expenses.findAndCountAll({
+        limit: 10,
+        offset: offset
+    }).then((expenses)=>{
+        //res.json(expenses.rows)
+        //console.log(expenses[0])
+        res.render("expenses/expenseslist",{
+            expenses: expenses.rows
+        })
+    })
+})
+
+router.get("/expenses/:id",(req, res) => {
+    //res.render("expenses/expense")
+    Expenses.findAndCountAll({
+        attributes: ['id','title','description'],
+        where: {
+            id: req.params.id
+        }
+    }).then((users)=>{
+        res.json(users)
+    })
 })
 
 router.post("/expenses/insert",(req, res) => {
-    console.log(req.body["title"])
-    console.log(req.body["value"])
-    console.log(req.body["description"])
+    title = req.body["title"]
+    value = req.body["value"]
+    description = req.body["description"]
+        Expenses.create({
+            title: title,
+            value: value,
+            description: description
+        })
 })
 
 module.exports = router
